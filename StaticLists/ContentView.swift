93fe847ae75515c0b17a9116a2ef8e9da1c9507a
Alec Meyer <11meyal@gmail.com>
2022-01-17 08:28:19 -0500

    Initial Commit

---
+++ StaticLists/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StaticLists
+//
+//  Created by Alec Meyer on 1/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StaticLists/StaticListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListsApp.swift
+//  StaticLists
+//
+//  Created by Alec Meyer on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct StaticListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
